9f16736eaf7c8e6bd3648f8fd6b9eafa7c5852fa
Jikjii <swegeraldogrell@gmail.com>
2026-02-06 02:28:31 -0500

    Add: email signup form and zod config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import {drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/drizzle/db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     emailAndPassword: {
@@ -9,9 +10,11 @@ export const auth = betterAuth({
     session: {
         cookieCache: {
             enabled: true,
-            maxAge: 60 * 5,
+            maxAge: 60 * 5, // 5 minutes
         }
     },
+
+    plugins: [nextCookies()],
 
     database: drizzleAdapter(db, {
         provider: "pg",
