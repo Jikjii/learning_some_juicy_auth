116c90751ca0ba87de651c2d18fe4989bbde18d5
Jikjii <swegeraldogrell@gmail.com>
2026-02-07 13:20:41 -0500

    Add: redirect to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         ): (
            <>
           <h1 className="text-3xl font-bold">Welcome {session.user.name}</h1>
-          <BetterAuthActionButton size="lg" variant="destructive" action={() => authClient.signOut()}>Sign Out</BetterAuthActionButton>
+          <BetterAuthActionButton size="lg" variant="destructive" successMessage="Signed out successfully" action={() => authClient.signOut()}>Sign Out</BetterAuthActionButton>
           </>
         )}
        
